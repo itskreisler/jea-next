da7f1f931b68ff630da9f6b0676461a61021d518
Kreisler Ramirez <itskreisler@outlook.com>
2023-02-11 18:14:41 -0500

    fin

--- hooks/chromium.js
+++ hooks/chromium.js
@@ -1,7 +1,15 @@
 import chromium from 'chrome-aws-lambda'
 export async function getBrowserInstance () {
   const executablePath = await chromium.executablePath
-
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require('puppeteer')
+    return puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      ignoreHTTPSErrors: true
+    })
+  }
   return chromium.puppeteer.launch({
     args: chromium.args,
     executablePath,
