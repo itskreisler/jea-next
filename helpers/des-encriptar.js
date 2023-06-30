215a4b57ba53d59fafd78202b3067e15025bada6
Kreisler Ramirez <itskreisler@outlook.com>
2023-06-30 16:16:21 -0500

    fix

--- helpers/des-encriptar.js
+++ helpers/des-encriptar.js
@@ -1,9 +1,10 @@
 import CryptoJS from 'crypto-js'
+const CRYPTO_KEY = process.env.CRYPTO_KEY || Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 export const cryptoJsEncrypt = (text, secretKey) => {
-  const ciphertext = CryptoJS.AES.encrypt(text, secretKey = 'secret-key')
+  const ciphertext = CryptoJS.AES.encrypt(text, secretKey = CRYPTO_KEY)
   return ciphertext.toString()
 }
-export const cryptoJsDecrypt = (ciphertext, secretKey = 'secret-key') => {
+export const cryptoJsDecrypt = (ciphertext, secretKey = CRYPTO_KEY) => {
   const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey)
   const originalText = bytes.toString(CryptoJS.enc.Utf8)
   return originalText
